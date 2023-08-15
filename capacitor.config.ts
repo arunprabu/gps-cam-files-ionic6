22b8f3bed27cae36a46cf398b3857ca06df8b800
Arun Vijayarengan <arunprabu.v@hotmail.com>
2023-08-15 13:05:13 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gps-cam-files-ionic6',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
